e5dd92d4a2544cfbae0e38527d604e075ed450ed
Tyler Watkins <tylerwatkins101@gmail.com>
2019-03-08 14:46:52 -0300

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -25,7 +25,7 @@ warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 
 def load_data(database_filepath):
-    # load data from database
+    ''' load data from database '''
     engine = create_engine('sqlite:///'+database_filepath)
     df = pd.read_sql('SELECT * FROM labelled_messages', engine)
     X = df.message
@@ -36,6 +36,7 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    ''' tokenize text data for tf-idf transformation '''
     tokens = RegexpTokenizer(r'\w+').tokenize(text)
     less_tokens = [w for w in tokens if w not in stopwords.words('english')]
     lemmatizer = WordNetLemmatizer()
@@ -50,6 +51,8 @@ def tokenize(text):
 
 def build_model():
     '''
+    Pipeline for building classification model
+    
     Parameters for model selected using GridSearchCV as follows:
     parameters = {
     'clf__estimator__n_estimators': [50, 100],
@@ -60,18 +63,18 @@ def build_model():
     cv.best_params_
 
     Code not run here because it takes over an hour to run on a single machine
-    Simpler model fit here to reduce model size on github
     '''
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators=100, min_samples_split=4)))
     ])
 
     return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    ''' Evaluate model and print precision, recall and f1-score for each category '''
     Y_pred = model.predict(X_test)
     for i in range(len(category_names)):
         print("Category: "+category_names[i])
@@ -79,6 +82,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    ''' Save model to pickle file '''
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
